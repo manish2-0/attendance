c6e9362b39dfb84d32efd69a00b861192ead8a56
manish2-0 <maishkumawat12955@gmail.com>
2023-04-18 15:49:19 +0530

    Changed the portal name

--- src/components/axiosapi.js
+++ src/components/axiosapi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //   baseURL:'https://blp-new-backend.vercel.app/',
   baseURL: 'https://blp-attendance-backend.vercel.app/',
+  // baseURL: 'http://localhost:8000/',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 })
